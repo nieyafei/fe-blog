37e71d556f99b6a1fff8249810b8694c2b0e6e42
nyf <327675933@qq.com>
2019-10-08 17:12:51 +0800

    update 初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  base: '/fe-blog/',
+  title: 'CODE QUN',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: 'Github', link: 'https://github.com/nieyafei/fe-blog' },
+    ]
+  }
+}
